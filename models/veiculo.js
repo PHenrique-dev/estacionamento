06302e765efe5e96ecb5ba0bcb8891b4fdb71f22
Pedro Henrique <100247785+PHenrique-dev@users.noreply.github.com>
2023-12-02 19:45:46 -0300

    Delete models directory

--- models/veiculo.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const veiculo = mongoose.model('Veiculo', {
-    nome: String,
-    marca: String,
-    modelo: Number,
-    approved: Boolean,
-});
-module.exports = veiculo
